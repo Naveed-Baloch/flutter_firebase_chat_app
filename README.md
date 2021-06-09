71d644804336dfb93ec54616ed32519aae8c2705
Muhammad Naveed <m.naveed32001@gmail.com>
2021-06-09 15:57:12 +0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# flutter_fireBase_chat_app
+ flutter project for basic understand and configuration of the firebase with the flutter that it works 
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,42 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Chat'),
+      ),
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection('chat/LIEYmGGQhPvsjJ6SBTMk/messages')
+            .snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting)
+            return CircularProgressIndicator();
+          else {
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              itemBuilder: (ctx, index) =>
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    child: Text(snapshot.data!.docs[index]['text']),
+                  ),
+            );
+          }
+        },
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.add),
+        onPressed: () async {},
+      ),
+    );
+  }
+}
